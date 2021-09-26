3bdf7058e985e079fd05c5df4f65ede9e6f85243
anhbc <anhbcse05256@gmail.com>
2021-09-26 23:39:56 +0700

    fix shadow card

--- lib/main_screen/card/components/shadow_card.dart
+++ lib/main_screen/card/components/shadow_card.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 
+// ignore: non_constant_identifier_names
 BoxDecoration shadow_Card() {
-    return BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          color: Colors.grey[300],
-          boxShadow: [
-            BoxShadow(
-                color: Colors.grey.shade500,
-                offset: Offset(4.0, 4.0),
-                blurRadius: 15.0,
-                spreadRadius: 1.0),
-            BoxShadow(
-                color: Colors.white,
-                offset: Offset(-4.0, -4.0),
-                blurRadius: 15.0,
-                spreadRadius: 1.0),
-          ]);
-  }
+  return BoxDecoration(
+      borderRadius: BorderRadius.circular(15),
+      color: Colors.grey[300],
+      boxShadow: [
+        BoxShadow(
+            color: Colors.grey.shade500,
+            offset: Offset(10.0, 10.0),
+            blurRadius: 15.0,
+            spreadRadius: 2.0),
+        BoxShadow(
+            color: Colors.white,
+            offset: Offset(-10.0, -10.0),
+            blurRadius: 15.0,
+            spreadRadius: 2.0),
+      ]);
+}
